4e04c9fc5ba48625f51cf3abe22ce528a376525f
=CoderMahmoud <115553302+CoderMahmoud@users.noreply.github.com>
2025-08-03 19:26:19 +0200

    feat: ➕ add user registration functionality
    - create IUserService interface
    - implement UserService class to handle user creation

---
+++ github-actions-demo.ConsoleApp/Services/Interfaces/IUserService.cs
@@ -0,0 +1,8 @@
+﻿using github_actions_demo.ConsoleApp.Contracts;
+
+namespace github_actions_demo.ConsoleApp.Services.Interfaces;
+
+internal interface IUserService
+{
+    void CreateUser(RegisterUserDto userDto);
+}
---
+++ github-actions-demo.ConsoleApp/Services/UserService.cs
@@ -0,0 +1,14 @@
+﻿using github_actions_demo.ConsoleApp.Contracts;
+using github_actions_demo.ConsoleApp.Models;
+using github_actions_demo.ConsoleApp.Repositories;
+using github_actions_demo.ConsoleApp.Services.Interfaces;
+
+namespace github_actions_demo.ConsoleApp.Services;
+
+internal class UserService(IUserRepository repository) : IUserService
+{
+    public void CreateUser(RegisterUserDto userDto)
+    {
+        repository.Insert(new User(userDto.UserName, userDto.Email));
+    }
+}

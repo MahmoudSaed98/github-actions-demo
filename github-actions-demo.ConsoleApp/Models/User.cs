6fb987f2cfe594de8bed02f1e0797292e736380f
=CoderMahmoud <115553302+CoderMahmoud@users.noreply.github.com>
2025-08-03 19:08:31 +0200

    Add project files.

---
+++ github-actions-demo.ConsoleApp/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace github_actions_demo.ConsoleApp.Models;
+
+public class User
+{
+    public User(string userName, string email)
+    {
+        UserName = userName;
+        Email = email;
+    }
+
+    public string UserName { get; set; }
+    public string Email { get; set; }
+}
---
+++ github-actions-demo.ConsoleApp/Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using github_actions_demo.ConsoleApp.Models;
+
+namespace github_actions_demo.ConsoleApp.Repositories;
+
+public interface IUserRepository
+{
+    void Insert(User user);
+    void Update(User user);
+    void Delete(User user);
+    User? GetByUserName(string userName);
+}
